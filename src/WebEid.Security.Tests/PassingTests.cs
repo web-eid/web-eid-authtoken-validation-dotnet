e8084510a3a3066d636799fc5d2388f8eec72aaa
Mati Agukas <mati.agukas@cgi.com>
2021-05-05 18:17:20 +0300

    Initial project setup

---
+++ src/WebEid.Security.Tests/PassingTests.cs
@@ -0,0 +1,18 @@
+namespace WebEID.Security.Tests
+{
+    using NUnit.Framework;
+
+    public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void PassingTest()
+		{
+			Assert.Pass();
+		}
+	}
+}
