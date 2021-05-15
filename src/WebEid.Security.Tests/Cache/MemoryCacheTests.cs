c089eabe344ca6e541c310dde49e62b3029e1949
Mati Agukas <mati.agukas@cgi.com>
2021-05-15 19:14:04 +0300

    feat: Add ICache interface

---
+++ src/WebEid.Security.Tests/Cache/MemoryCacheTests.cs
@@ -0,0 +1,47 @@
+namespace WebEID.Security.Tests.Cache
+{
+    using System;
+    using System.Threading;
+    using NUnit.Framework;
+    using Security.Cache;
+
+    [TestFixture]
+    public class MemoryCacheTests
+    {
+        /// <summary>
+        /// 1. Cache the value
+        /// 2. Obtain and remove the value from the cache and make sure it is correct
+        /// 3. Make sure the value is removed from the cache
+        /// </summary>
+        [Test]
+        public void CacheItemIsAddedAndRemoved()
+        {
+            var cacheKey = "CACHE_KEY";
+            var cacheValue = "CACHE_VALUE";
+            using (ICache<string> cache = new MemoryCache<string>())
+            {
+                cache.Put(cacheKey, cacheValue);
+                Assert.AreEqual(cacheValue, cache.GetAndRemove(cacheKey));
+                Assert.IsNull(cache.GetAndRemove(cacheKey));
+            }
+        }
+
+        /// <summary>
+        /// 1. Cache the value
+        /// 2. Wait for the value to expire
+        /// 3. Make sure the value has expired
+        /// </summary>
+        [Test]
+        public void CacheItemIsExpired()
+        {
+            var cacheKey = "CACHE_KEY";
+            var cacheValue = "CACHE_VALUE";
+            using (ICache<string> cache = new MemoryCache<string>(TimeSpan.FromMilliseconds(10)))
+            {
+                cache.Put(cacheKey, cacheValue);
+                Thread.Sleep(10);
+                Assert.IsNull(cache.GetAndRemove(cacheKey));
+            }
+        }
+    }
+}
