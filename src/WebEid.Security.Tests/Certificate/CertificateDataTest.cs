7bbe95dda9aefa4fb20edea3d5348a24b606034a
Erkki Arus <erkki.arus@gmail.com>
2023-07-05 15:39:48 +0300

    Adds support of organisation certificate
    Added organisation certificate data extraction and appropriate tests.
    
    Signed-off-by: Erkki Arus <erkki@raulwalter.com>

---
+++ src/WebEid.Security.Tests/Certificate/CertificateDataTest.cs
@@ -0,0 +1,28 @@
+namespace WebEid.Security.Tests.Certificate
+{
+    using System.Security.Cryptography.X509Certificates;
+    using NUnit.Framework;
+    using WebEid.Security.Exceptions;
+    using WebEid.Security.Tests.TestUtils;
+    using WebEid.Security.Util;
+
+    public class CertificateTests
+    {
+        [Test]
+        public void WhenOrganizationCertificateThenSubjectCNAndIdCodeAndCountryCodeExtractionSucceeds()
+        {
+            X509Certificate organizationCert = Certificates.GetOrganizationCert();
+            Assert.That(organizationCert.GetSubjectCn(), Is.EqualTo("Testijad.ee isikutuvastus"));
+            Assert.That(organizationCert.GetSubjectIdCode(), Is.EqualTo("12276279"));
+            Assert.That(organizationCert.GetSubjectCountryCode(), Is.EqualTo("EE"));
+        }
+
+        [Test]
+        public void WhenOrganizationCertificateThenSubjectGivenNameAndSurnameExtractionFails()
+        {
+            X509Certificate organizationCert = Certificates.GetOrganizationCert();
+            Assert.Throws<CertificateEncodingException>(() => organizationCert.GetSubjectGivenName());
+            Assert.Throws<CertificateEncodingException>(() => organizationCert.GetSubjectSurname());
+        }
+    }
+}
