076734ccc22d977e3432b24ce00a66f980ad7c69
Sander.Kondratjev <sander.kondratjev@nortal.com>
2025-11-27 14:53:07 +0200

    NFC-99 Update licence dates Signed-off-by: Sander Kondratjev <sander.kondratjev@nortal.com>

--- example/src/WebEid.AspNetCore.Example/Controllers/Api/MobileAuthLoginController.cs
+++ example/src/WebEid.AspNetCore.Example/Controllers/Api/MobileAuthLoginController.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2021-2024 Estonian Information System Authority
+﻿// Copyright (c) 2025-2025 Estonian Information System Authority
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
